b14a50ae9d7e3df602babd5c13a45e702d7b4278
Joseph Evangelista <oootep123@gmail.com>
2019-08-05 23:59:47 -0400

    implemented mongoose queries and connection tested

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator');
+
+// User Model
+const User = require('../../models/user');
 
 // @route POST api/users
 // @desc Register user
@@ -17,21 +22,51 @@ router.post(
       'Please enter a password with 6 or more characters'
     ).isLength({ min: 6 })
   ],
-  (req, res) => {
+  async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+    const { name, email, password } = req.body;
+
+    // mongooses query
 
     // See if the user exist
+    try {
+      let user = await User.findOne({ email });
 
-    // Get users gravatar
+      if (user) {
+        res.status(400).json({ erros: [{ msg: 'User already exists' }] });
+      }
 
-    // Encrypt password
+      // Get users gravatar
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm'
+      });
 
-    //  Return jsonwebtoken
+      user = new User({
+        name,
+        email,
+        avatar,
+        password
+      });
 
-    res.send('User route');
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+       await user.save();
+
+      //  Return jsonwebtoken
+
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 

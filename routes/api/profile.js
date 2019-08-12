20dfc5f05ca63901886f9a47043dc54e4a2f71e2
Joseph Evangelista <oootep123@gmail.com>
2019-08-11 23:54:35 -0400

    implemented create and update user route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,27 +1,54 @@
 const express = require('express');
 const router = express.Router();
-const auth = require("../../middleware/auth");
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-const Profile = require("../../models/Profile");
-const User = require("../../models/User");
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
 
 // @route GET api/profile/me
 // @desc Get current users profile
 // @access Private
 router.get('/me', auth, async (req, res) => {
-    try {
-        const profile = await Profile.findOne({user: req.user.id}).populate(
-            "user", 
-            ["name", "avatar"]);
+  try {
+    const profile = await Profile.findOne({ user: req.user.id }).populate(
+      'user',
+      ['name', 'avatar']
+    );
 
-        if(!profile) {
-            return res.status(400).json({msg: "There is no profile for this user"})
-        }
-        res.json(profile);
-    } catch(err) {
-        console.error(err.message);
-        res.status(500).send("Server error")
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
     }
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
 });
 
+// @route Post api/profile/me
+// @desc Create or update user profile
+// @access Private
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('status', 'Status is required')
+        .not()
+        .isEmpty(),
+      check('skills', 'Skills is required')
+        .not()
+        .isEmpty()
+    ]
+  ],
+  async (req, res) => {
+    const error = validationResult(req);
+    if (!erros.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+  }
+);
+
 module.exports = router;
+

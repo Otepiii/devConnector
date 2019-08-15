a49a8170888636146ac56bd3a9d536bd53e0a909
Joseph Evangelista <oootep123@gmail.com>
2019-08-15 00:03:00 -0400

    added profileFields

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -43,12 +43,42 @@ router.post(
     ]
   ],
   async (req, res) => {
-    const error = validationResult(req);
-    if (!erros.isEmpty()) {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
+
+    const {
+      company,
+      website,
+      location,
+      bio,
+      status,
+      githubusername,
+      skills,
+      youtube,
+      facebook,
+      twitter,
+      instagram,
+      linkedin
+    } = req.body;
+
+    //Build profile object
+    const profileFields = {};
+    profileFields.user = req.user.id;
+    if (company) profileFields.company = company;
+    if (website) profileFields.website = website;
+    if (location) profileFields.location = location;
+    if (bio) profileFields.bio = bio;
+    if (status) profileFields.status = status;
+    if (githubusername) profileFields.githubusername = githubusername;
+    if (skills) {
+      profileFields.skills = skills.split(',').map(skill => skill.trim());
+    }
+    console.log(skills);
+
+    res.send('hello');
   }
 );
 
 module.exports = router;
-

eb32b93fe994845d5cdbb6fb95058d0a00750226
Joseph Evangelista <oootep123@gmail.com>
2019-08-16 16:32:26 -0400

    Implemented Get all profiles and get profiles by user id

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -114,4 +114,38 @@ router.post(
   }
 );
 
+// @route Get api/profile
+// @desc get all profiles
+// @access Public
+router.get('/', async (req, res) => {
+  try {
+    const profiles = await Profile.find().populate('user', ['name', 'avatar']);
+    res.json(profiles);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
+// @route Get api/profile/user/user_id
+// @desc get profile by user ID
+// @access Public
+router.get('/user/:user_id', async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.params.user_id
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) return res.status(400).json({ msg: 'Rrofile not found' });
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    if ((err.kind = 'ObjectId')) {
+      return res.status(400).json({ msg: 'Profile not found' });
+    }
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;

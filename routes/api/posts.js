fd58d406734617e3956cf33829308e3349d9025d
Joseph Evangelista <oootep123@gmail.com>
2019-08-20 14:54:43 -0400

    Changed something in the post model and implemented like a post route

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -106,4 +106,30 @@ router.delete('/:id', auth, async (req, res) => {
     res.status(500).send('Server error');
   }
 });
+
+// @route PUT api/posts/like/:id
+// @desc like a post
+// @access Private
+router.put('/like/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if the post has already been liked
+    if (
+      post.likes.filter(like => like.id.toString() === req.user.id).length > 0
+    ) {
+      return res.status(400).json({ msg: 'Post already liked' });
+    }
+
+    post.likes.unshift({ user: req.user.id });
+
+    await post.save();
+
+    res.json(post.likes);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server error');
+  }
+});
+
 module.exports = router;

1ce4e860ccb94292c8172e516b63dc0750a8ca41
joseph Evangelista <oootep123@gmail.com>
2019-12-02 22:11:52 -0500

    Protected route for dashboard

---
+++ client/build/precache-manifest.324eaed5de74e5df49ffc3321ccfee8e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9e6a60dd64c1a36f9461b0b706e3d53",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5eb298d73dc063eb22",
+    "url": "/static/css/main.7620ecbe.chunk.css"
+  },
+  {
+    "revision": "a5bfcae2696587952731",
+    "url": "/static/js/2.d8c5026f.chunk.js"
+  },
+  {
+    "revision": "6d5eb298d73dc063eb22",
+    "url": "/static/js/main.669d6506.chunk.js"
+  },
+  {
+    "revision": "83e7f8b54e61b1cbddf8",
+    "url": "/static/js/runtime~main.e38743dc.js"
+  },
+  {
+    "revision": "edf672b40cd91a953963cd3c0cc70a1b",
+    "url": "/static/media/Dual Ring-1s-200px.edf672b4.gif"
+  },
+  {
+    "revision": "c4838335fbf2dec536ae4c68950bda6e",
+    "url": "/static/media/device-electronics-keyboard-872958.c4838335.jpg"
+  }
+]);

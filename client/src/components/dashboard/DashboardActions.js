11e75648870bd110129d9ed507332cca90355339
joseph Evangelista <oootep123@gmail.com>
2019-12-08 14:09:14 -0500

    implemented edit profile

---
+++ client/src/components/dashboard/DashboardActions.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+export const DashboardActions = () => {
+  return (
+    <div>
+      <div className="dash-buttons">
+        <Link to="/edit-profile" className="btn btn-success">
+          <i className="fas fa-user-circle text-light"></i> Edit Profile
+        </Link>
+        <Link to="/add-experience" className="btn btn-success">
+          <i className="fab fa-black-tie text-light"></i> Add Experience
+        </Link>
+        <Link to="/add-education" className="btn btn-success">
+          <i className="fas fa-graduation-cap text-light"></i> Add Education
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default DashboardActions;
